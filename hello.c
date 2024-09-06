9fccd8cdc85373e72ff3cef7d4dcd88b11ef0b15
Jonathan <jon.kazmaier1@gmail.com>
2024-09-06 13:57:59 -0500

    Change hello to bye

--- hello.c
+++ hello.c
@@ -9,6 +9,6 @@ Write your code in this editor and press "Run" button to compile and execute it.
 #include <stdio.h>
 int main()
 {
-    printf("%s\n", "hello world");
+    printf("%s\n", "bye world");
     return 0;
 }
